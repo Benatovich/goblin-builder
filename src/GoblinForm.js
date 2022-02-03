f99bc37968224dfdba631ec9b1467dc790418ce9
Benatovich <sambenatovich@gmail.com>
2022-02-02 19:31:14 -0500

    GoblinForm component is working

--- src/GoblinForm.js
+++ src/GoblinForm.js
@@ -1,30 +1,36 @@
 import React from 'react';
 
 export default function GoblinForm(props) {
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newGoblin = {
+      name: props.goblinFormName,
+      hp: props.goblinFormHP,
+      color: props.goblinFormColor,
+    };
+
+    props.submitGoblin(newGoblin);
+
+    props.setGoblinFormName('');
+    props.setGoblinFormHP('');
+    props.setGoblinFormColor('');
+  }
+
   return (
     <div className='goblin-form-container quarter'>
-      {/* on submit, call the submitGoblin function, passed in as a prop.
-      Note that you don't need to define an anonymous function--you can just name the submitGoblin prop here and it will work.
-      Take a minute to try and puzzle out why that is: how is this function different from other functions, 
-      where you do need to create an anonymous function in the `onClick` or `onSubmit` spot? */}
-      <form className='goblin-form'>
+      <form className='goblin-form' onSubmit={handleSubmit}>
         <label>
             Name
-          {/* onChange, use the prop setGoblinFormName to set the parent state */}
-          <input required value={props.goblinFormName} />
-          {/* note that we're controlling the input's value from parent state */}
+          <input required value={props.goblinFormName} onChange={e => props.setGoblinFormName(e.target.value)}/>
         </label>
         <label>
             HP
-          {/* onChange, use the prop setGoblinFormHP to set the parent state */}
-          <input required type="number" value={props.goblinFormHP} />
-          {/* note that we're controlling the input's value from parent state */}
+          <input required type="number" value={props.goblinFormHP} onChange={e => props.setGoblinFormHP(e.target.value)}/>
         </label>
         <label>
             Color
-          {/* onChange, use the prop setGoblinFormColor to set the parent state */}
-          <select required value={props.goblinFormColor}>
-            {/* note that we're controlling the input's value from parent state */}
+          <select required value={props.goblinFormColor} onChange={e => props.setGoblinFormColor(e.target.value)}>
             <option value="lightgreen">Green</option>
             <option value="lightblue">Blue</option>
             <option value="pink">Pink</option>
